f7f44b683e7f19f228661cabd928f5a2f146052e
PhanQuocKhanh29 <88721369+PhanQuocKhanh29@users.noreply.github.com>
2021-10-26 10:38:40 +0700

    Add project files.

---
+++ BTL_MVC/Migrations/202110260213333_CreateTable_Person.cs
@@ -0,0 +1,26 @@
+﻿namespace BTL_MVC.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateTable_Person : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Person",
+                c => new
+                    {
+                        PersonID = c.String(nullable: false, maxLength: 128, unicode: false),
+                        PersonName = c.String(),
+                    })
+                .PrimaryKey(t => t.PersonID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Person");
+        }
+    }
+}
---
+++ BTL_MVC/Models/QLDVDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+
+namespace BTL_MVC.Models
+{
+    public partial class QLDVDbContext : DbContext
+
+    {       
+        public QLDVDbContext() : base("name=QLDVDbContext")
+        {
+        }
+        public virtual DbSet<Person> People { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Person>()
+            .Property(e => e.PersonID)
+            .IsUnicode(false);
+        }
+    }
+
+}
